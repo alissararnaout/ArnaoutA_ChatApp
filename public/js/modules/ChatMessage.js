be3ffdb14d84a67b84d72776b99cd9a41f126d9d
Alissar Arnaout <Alissar@Alissars-Air.fc.ca>
2020-01-29 10:15:51 -0500

    chat running

--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -2,5 +2,19 @@
 // will be made public when you import this file into another via the import statement
 
 export default {
+    props: ['msg'], // we're passing data through this component
+
+    template: `
+        <li>
+            <p class="new-message">
+                <span>{{ msg.message.name}} says:</span>
+                {{ msg.message.content }}
+            </p>
+        </li>
+    `,
+
+    data: function() {
+        return {message: "hello from the template"};
+    } 
 
 }
