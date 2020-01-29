4a88749a09863b07aae110df9c01678c27974824
Alissar Arnaout <Alissar@Alissars-Air.fc.ca>
2020-01-29 08:44:11 -0500

    adding socket + connections

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,17 @@
 // imports always go first - if we're importing anything
+
+const socket = io(); // instantiate library on the client side
+
+// the packet is whatever data we send through with the connect event
+// from the server
+function setUserId(packet) {
+    // debugger;
+    console.log(packet);
+}
+
+function showDisconnectMessage(){
+    console.log('a user disconnected');
+}
+
+socket.addEventListener('connected', setUserId); // listen to 'connected' event on the server
+socket.addEventListener('disconnect', showDisconnectMessage)
