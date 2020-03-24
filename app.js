8a159d13aaa3a59a6250373dda24356a969c191e
Alissar Arnaout <Alissar@Alissars-MacBook-Air.local>
2020-03-24 15:27:21 -0400

    build files debug

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ io.on('connection', function(socket) { // socket is our connection
     console.log('a user has connected');
     socket.emit('connected', {sID: socket.id, message: "new connection"}); // this is the packet we're passing through the event
 
+    socket.on('chat_message', function(msg) {
+        console.log(msg); // see what the payload is from the client side
+
+        // tell connection manager (io) to send this message to everyone
+        // anyone connected to our chat app will get this message - including the sender
+        io.emit('new_message', {id: socket.id, message: msg})
+    })
+
     socket.on('disconnect', function() {
         console.log('a user has disconnected');
     })
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,17 +1,60 @@
-// imports always go first - if we're importing anything
+// imports always go first
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io(); // instantiate library on the client side
 
 // the packet is whatever data we send through with the connect event
 // from the server
-function setUserId(packet) {
+
+// this is data destructuring 
+function setUserId({sID}) {
     // debugger;
-    console.log(packet);
+    console.log(sID);
+    vm.socketID = sID; 
 }
 
 function showDisconnectMessage(){
     console.log('a user disconnected');
 }
 
+function appendMessage(message) {
+    vm.messages.push(message);
+}
+
+const vm = new Vue({
+    data: {
+        socketID: "",
+        message: "",
+        nickname: "",
+        messages: []
+    },
+
+    methods: {
+        // emit a message event to the server so that it can then send this to anyone who's connected
+        dispatchMessage() {
+            console.log('handle emit message');
+
+            // double pipe || is an "or" operator
+            // if first value is set, use it
+            // else use whatever comes after the "or" operator
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickname || "anonymous"
+            })
+
+            this.message = "";
+        }
+    },
+
+    mounted: function() {
+        console.log('vue is done mounting');
+    },
+
+    components: {
+        newmessage: ChatMessage
+    }
+}).mount("#app");
+
 socket.addEventListener('connected', setUserId); // listen to 'connected' event on the server
 socket.addEventListener('disconnect', showDisconnectMessage)
+socket.addEventListener('new_message', appendMessage);
